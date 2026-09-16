f61dcd8b8c63bef9adf82ea7041076a1ce974ebb
dhirendraj-cmd <djha5051@gmail.com>
2026-09-16 11:09:02 +0530

    added append function

--- ministrbldr/stringbldr.c
+++ ministrbldr/stringbldr.c
@@ -1,6 +1,7 @@
 #include "stringbldr.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
@@ -14,7 +15,7 @@ StringBldr* create_str(size_t initialCapacity){
     
     // stringbld allocation to heap memory
     StringBldr *sb = malloc(sizeof(StringBldr));
-    
+
     // if not memory then return null
     if (!sb){
         return NULL;
@@ -35,5 +36,63 @@ StringBldr* create_str(size_t initialCapacity){
 
     return sb;
 
+}
+
+
+// get length of str passsed in append usaing custom func instead of strlen
+int get_length(const char *str){
+    int length=0;
+
+    while (str[length]!='\0')
+    {
+        length++;
+    }
+    return length;
+}
+
+
+void append_str(StringBldr *sb, const char *str){
+    // if not str passed or not sb is there
+    if (!sb || !str){
+        return ;
+    }
+
+
+    size_t textlength;
+    size_t neededSize;
+
+    // get current text length
+    textlength = get_length(str);
+    printf("Length of current text is: %d", textlength);
+
+    // create required size
+    neededSize = sb->lengthTracker + textlength + 1;
+
+
+    // check required size is more than capacitytracker
+    if(neededSize > sb->capacityTracker){
+
+        // multiply capacitytracker by 2 until capacity exceeds neededsize
+        while (neededSize > sb->capacityTracker)
+        {
+            sb->capacityTracker *= 2;
+        }
+
+        // finally reallocate new memory
+        char *newBuffmemo = realloc(sb->buffermemo, sb->capacityTracker);
+
+        if (!newBuffmemo){
+            return;
+        }
+
+        sb->buffermemo = newBuffmemo;
+
+    }
+
+    // if nneededsize < capacitytracker then copy in buffer
+    strcpy(sb->buffermemo + sb->lengthTracker, str);
+    sb->lengthTracker+=textlength;
+
+    
 }
 
